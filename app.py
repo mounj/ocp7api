75be5dd1fea6b19be8cd317e1e3791c5adf9885b
Julien MOURIN <julien.mourin@gmail.com>
2022-10-03 17:44:58 +0200

    URL update

--- app.py
+++ app.py
@@ -63,7 +63,8 @@ def predict_defautclient(data: BankClient):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    # uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='https://ocp7gitapi.herokuapp.com/', port=8000)
 
 #uvicorn app:app --reload
 # requirement uvlopp
